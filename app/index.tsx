ce2d9c542bf3c48cb36c8448610a9b5e491b9bf3
Jie-Si Chen <jessie@paxai.com>
2026-02-05 13:43:44 +0800

    add movie details page

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary font-bold">Welcome</Text>
-      <Link href="/onboarding">Onboarding</Link>
+      <Text className="text-5xl text-primary font-bold">Welcome!</Text>
+        <Link href="/onboarding">Onboarding</Link>
+        <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
