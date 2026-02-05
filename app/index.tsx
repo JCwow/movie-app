5e00010922104728068cc28f8169cfcc1f9b85d2
Jie-Si Chen <jessie@paxai.com>
2026-02-05 13:31:23 +0800

    implement index page

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 f">
-      <Text></Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Welcome</Text>
     </View>
   );
 }
